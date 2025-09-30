8db39689808e62f38c715e1e34c28d2f41587667
VinishaGupta <vinishasgupta@gmail.com>
2025-09-30 12:40:28 +0530

    7

---
+++ quiz/script.js
@@ -0,0 +1,9 @@
+function openFun(){
+    document.querySelector(".sidebar").style.width='300px';
+    document.querySelector(".question-container").style.leftMargin='300px';
+}
+
+function closeFun(){
+    document.querySelector(".sidebar").style.width='0px';
+    document.querySelector(".question-container").style.leftMargin='0px';
+}
